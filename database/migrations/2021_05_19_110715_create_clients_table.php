c14278736384dbd6640fb354a02ff74c49e6586b
Minas Sarkisyan <dev3@alvariumsoft.dev>
2021-05-19 17:46:18 +0300

    Create clients and sms tables

---
+++ database/migrations/2021_05_19_110715_create_clients_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->text('full_name');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->string('password');
+            $table->timestamp('birthday');
+            $table->text('address');
+            $table->text('note');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
